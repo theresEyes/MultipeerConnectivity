7de89e7edb12d444db8252e0b62c357839423038
QiZhang <qizhang@tvunetworks.com>
2016-07-18 17:26:33 +0800

    Complete ConnectionsViewController

--- MCDemo/MCDemo/ConnectionsViewController.h
+++ MCDemo/MCDemo/ConnectionsViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ConnectionsViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UISwitch *swVisible;
+@property (weak, nonatomic) IBOutlet UITextField *txtName;
+@property (weak, nonatomic) IBOutlet UIButton *btnDisconnect;
 
 @end
