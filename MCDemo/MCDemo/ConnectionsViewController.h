c8664d61eae561d429627c41f55ad2f10b9be9c1
QiZhang <qizhang@tvunetworks.com>
2016-07-18 15:51:58 +0800

    Init UI

---
+++ MCDemo/MCDemo/ConnectionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConnectionsViewController.h
+//  MCDemo
+//
+//  Created by zhangqi on 18/7/2016.
+//  Copyright © 2016 zhangqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConnectionsViewController : UIViewController
+
+@end
---
+++ MCDemo/MCDemo/ConnectionsViewController.m
@@ -0,0 +1,13 @@
+//
+//  ConnectionsViewController.m
+//  MCDemo
+//
+//  Created by zhangqi on 18/7/2016.
+//  Copyright © 2016 zhangqi. All rights reserved.
+//
+
+#import "ConnectionsViewController.h"
+
+@implementation ConnectionsViewController
+
+@end
