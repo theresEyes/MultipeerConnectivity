1e88ab9076aaf3cfdc44d54671026b087192598c
QiZhang <qizhang@tvunetworks.com>
2016-07-18 14:33:38 +0800

    Init code

---
+++ MCDemo/MCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCDemo
+//
+//  Created by zhangqi on 18/7/2016.
+//  Copyright © 2016 zhangqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MCDemo/MCDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MCDemo
+//
+//  Created by zhangqi on 18/7/2016.
+//  Copyright © 2016 zhangqi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
